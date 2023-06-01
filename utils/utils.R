0f83d8e32fd081b6e6eda914c680416927a48eb0
MirkoTh <mirkothalmann@hotmail.com>
2023-06-01 17:35:33 +0200

    add bias parameter to gcm_base

--- utils/utils.R
+++ utils/utils.R
@@ -1,22 +1,24 @@
-gcm_base <- function(x_new, tbl_x, n_feat, c, w, delta, d_measure = 1){
+gcm_base <- function(x_new, tbl_x, n_feat, c, w, bias, delta, d_measure = 1){
   #' compute class probabilities with the GCM model
   #' 
   #' @description summed similarity computation with gcm;
-  #' using sensitivity and attentional weighting;
-  #' currently, 
-  #' response bias is not implemented
+  #' using sensitivity, attentional weighting, and response bias;
   #' @param x_new the x coordinates of the new item
   #' @param tbl_x the tbl with all memory exemplars,
   #' including a column "category" denoting the category of that item
   #' @param n_feat number of feature dimensions
-  #' @param w attentional weighting
   #' @param c sensitivity
+  #' @param w attentional weighting
+  #' @param bias response bias / category prior
   #' @param delta forgetting rate (if delta == 0, no forgetting)
   #' @return a vector with the class probabilities for the new item
+  w <- c(w, 1 - w)
+  bias <- c(bias, 1 - bias)
   l_x_cat <- split(tbl_x, tbl_x$category)
   sims_cat <- map(l_x_cat, f_similarity_cat, w, c, delta, x_new, d_measure)
-  map_dbl(sims_cat, ~ sum(.x)/sum(map_dbl(sims_cat, sum)))
-  
+  sims_cat_sum <- map_dbl(sims_cat, sum)
+  sims_cat_sum_biased <- sims_cat_sum * bias
+  map_dbl(sims_cat_sum_biased, ~ .x/sum(sims_cat_sum_biased))
 }
 
 f_similarity <- function(x1, x2, w, c, x_new, d_measure) {
@@ -81,9 +83,6 @@ upsample <- function(category_used, n_upsample, tbl_x) {
   }
   tbl_upsample <- rbind(unique_points, new_points)
   tbl_upsample$category <- category_used
-  # tbl_upsample <- tbl_upsample %>%
-  #   group_by(x1, x2, category) %>%
-  #   count() %>% dplyr::select(-n)
   
   return(tbl_upsample)
 }
@@ -96,7 +95,7 @@ x_new <- tibble(x1 = 4.5, x2 = 4.7)
 tbl_x <- tbl_x_ii
 n_feat <- 2
 c <- .5
-w <- rep(1/n_feat, n_feat) # equal
+w <- 1/n_feat # equal
 delta <- .99
 d_measure <- 1
 
@@ -127,18 +126,57 @@ gcm_base(x_new, tbl_upsample, 2, c, w, 0, d_measure)
 
 # number of finally used items is controlled by capacity parameter
 
+tbl_x_ii_inb$response <- tbl_x_ii_inb$category
+
+# category 0 example
+gcm_base(tbl_x_ii_inb[1, c("x1", "x2")], tbl_x_ii_inb[-1, ], 2, c = .5, w = c(.5, .5), 0, d_measure = 1)
+# category 1 example
+gcm_base(tbl_x_ii_inb[17, c("x1", "x2")], tbl_x_ii_inb, 2, c = 3, w = c(.001, .999), 0, d_measure = 1)
 
 
+# example using inbalanced data set sequentially presenting data
 
+tbl_inb <- tbl_x_ii %>% 
+  filter(
+    category %in% c(0) |
+      (category %in% c(1) & x1 %in% seq(1, 9, by = 2) & x2 %in% seq(2, 10, by = 2))
+  )
+plot_grid(tbl_inb) + geom_abline()
 
+points_new <- tibble(
+  x1 = seq(1.5, 9.5, by = 1),
+  x2 = x1 + .5,
+  category = 3
+)
 
+plot_grid(rbind(tbl_inb %>% dplyr::select(-c(cat_structure, trial_id)), points_new %>% mutate(category = factor(category)))) + geom_abline()
 
+gcm_base(points_new[1, c("x1", "x2")], tbl-inb, c = 1, w = c(.5, .5), delta = 0, d_measure = 1)
 
+gcm_base(tibble(x1 = 2, x2 = 3), tbl_inb, c = 1, w = .5, delta = 0, d_measure = 1)
+# for one category
+categories <- c(0, 1)
+ns_upsample <- c(0, 105)
 
+l_tbl_upsample_inb <- map2(categories, ns_upsample, upsample, tbl_x = tbl_inb)
+tbl_inb_upsample <- l_tbl_upsample_inb %>% reduce(rbind) %>% 
+  group_by(x1, x2, category) %>%
+  count() %>% dplyr::select(-n) %>%
+  ungroup() %>%
+  mutate(trial_id = seq(1, nrow(.)))
 
+plot_grid(tbl_inb_upsample %>% mutate(category = factor(category)))
 
+gcm_base(tibble(x1 = 2.5, x2 = 4), tbl_inb, 2, c = 1, w = .5, bias = 1/3, delta = 0, d_measure = 1)
 
+# todo
+# create grid of 10 x 10 values crossing x1 and x2
+# 1. run gcm with bias parameter varying from unbiased to very biased and plot probabilities category 0 over 100 grid points
+# 2. run gcm with upsampled data sets (few added points - many added points)
 
+# main remaining issue: how is upsampling exactly achieved in the model? given I want to add n data points to one category
+# possible solution: do upsampling as currently implemented in upsample function, and then rank sampled data points according to their importance for the categorization problem
+# only keep the n most important data points
 
 
 
