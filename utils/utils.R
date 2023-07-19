bb750e7c4965d0a48a06ecb21d88459c5685cab5
MirkoTh <mirkothalmann@hotmail.com>
2023-07-19 14:18:54 +0200

    add fs to constrain parameter space

--- utils/utils.R
+++ utils/utils.R
@@ -130,6 +130,8 @@ gcm_likelihood_no_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measu
   #' @param n_feat number of features
   #' @param d_measure distance measure, 1 for city-block, 2 for euclidean
   #' @return negative 2 * summed log likelihood
+  #' 
+  x <- pmap(list(x, c(0, 0, 0), c(100, 1, 1)), upper_and_lower_bounds_revert)
   l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
   l_category_probs <- map(l_transfer_x, gcm_base, tbl_x = tbl_x, n_feat = n_feat, c = x[[1]], w = x[[2]], bias = x[[3]], delta = 0, d_measure = d_measure)
   tbl_probs <- as.data.frame(reduce(l_category_probs, rbind)) %>% mutate(category = tbl_transfer$category)
@@ -152,6 +154,8 @@ gcm_likelihood_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measure)
   #' @param n_feat number of features
   #' @param d_measure distance measure, 1 for city-block, 2 for euclidean
   #' @return negative 2 * summed log likelihood
+  #' 
+  x <- pmap(list(x, c(0, 0, 0), c(100, 1, 1)), upper_and_lower_bounds_revert)
   l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
   l_category_probs <- map(l_transfer_x, gcm_base, tbl_x = tbl_x, n_feat = n_feat, c = x[[1]], w = x[[2]], bias = x[[3]], delta = x[[4]], d_measure = d_measure)
   tbl_probs <- as.data.frame(reduce(l_category_probs, rbind)) %>% mutate(category = tbl_transfer$category)
@@ -230,4 +234,13 @@ importance_downsampling <- function(tbl_inb_plus, params, tbl_transfer, n_feat, 
   
   future::plan("default")
   return(tbl_drop)
+}
+
+
+upper_and_lower_bounds <- function(par, lo, hi) {
+  log(((par - lo) / (hi - lo)) / (1 - (par - lo) / (hi - lo)))
+}
+
+upper_and_lower_bounds_revert <- function(par, lo, hi) {
+  lo + ((hi - lo) / (1 + exp(-par)))
 }
