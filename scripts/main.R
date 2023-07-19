62e225d5edd3ea1d7f2647e924be19ef5570f900
MirkoTh <mirkothalmann@hotmail.com>
2023-07-19 09:58:54 +0200

    move fs to utils and add documentation

--- scripts/main.R
+++ scripts/main.R
@@ -5,6 +5,8 @@ library(smotefamily)
 library(furrr)
 library(MASS)
 
+path_load <- c("utils/utils.R")
+walk(path_load, source)
 
 # create two category structures ------------------------------------------
 
@@ -21,16 +23,6 @@ tbl_x_ii <- tbl_x_ii[!(tbl_x_ii$x1 == tbl_x_ii$x2), ]
 tbl_x_ii$cat_structure <- "information-integration"
 tbl_x_ii$trial_id <- seq(1, nrow(tbl_x_ii), by = 1)
 
-plot_grid <- function(tbl) {
-  ggplot(tbl, aes(x1, x2, group = category)) + 
-    geom_point(aes(color = category)) + 
-    theme_bw() +
-    scale_x_continuous(expand = c(.1, .1), breaks = seq(2, 10, by = 2)) +
-    scale_y_continuous(expand = c(.1, .1), breaks = seq(2, 10, by = 2)) +
-    labs(x = expression(x[1]), y = expression(x[2])) +
-    theme(strip.background = element_rect(fill = "white")) +
-    scale_color_viridis_d(name = "Category")
-}
 pl_ii <- plot_grid(tbl_x_ii) + geom_abline()
 
 
@@ -69,12 +61,6 @@ tbl_x_ii_inb$trial_id <- seq(1, nrow(tbl_x_ii_inb), by = 1)
 plot_grid(tbl_x_ii_inb) + geom_abline()
 
 
-sample_2d_stimuli <- function(x1, x2, category, n, sd) {
-  s <- mvrnorm(n, c(x1, x2), matrix(c(sd, 0, 0, sd), nrow = 2))
-  tbl <- tibble(as.data.frame(s), category)
-  colnames(tbl) <- c("x1", "x2", "category")
-  return(tbl)
-}
 
 l_samples_ii <- pmap(tbl_x_ii_inb[, c("x1", "x2", "category", "n")], sample_2d_stimuli, sd = .2)
 tbl_samples_ii <- reduce(l_samples_ii, rbind)
@@ -119,5 +105,220 @@ l_smote <- map(c(5, 10, 20), smote_k)
 
 grid.draw(arrangeGrob(pl_bl, l_smote[[1]], l_smote[[2]], l_smote[[3]], nrow = 2))
   
+
+
+# Base Models -------------------------------------------------------------
+
+
+tbl_inb <- tbl_x_ii %>% 
+  filter(
+    category %in% c(0) |
+      (category %in% c(1) & x1 %in% seq(1, 9, by = 4) & x2 %in% seq(2, 10, by = 4))
+  )
+pl_inb <- plot_grid(tbl_inb %>% mutate(category = factor(category))) + ggtitle("Stimuli") + geom_abline()
+
+
+# the base models use all presented data points to predict on the transfer set
+
+# create 10 x 10 grid of data points as a transfer set
+xs <- seq(1, 10, by = 1)
+tbl_transfer <- crossing(x1 = xs, x2 = xs) %>% mutate(category = fct_rev(factor(x1 > x2, labels = c(1, 0))))
+l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
+plot_grid(tbl_transfer) + geom_abline()
+
+
+# base model with bias term = .5
+l_category_probs.5 <- map(l_transfer_x, gcm_base, tbl_x = tbl_inb, n_feat = 2, c = 1, w = .5, bias = .5, delta = 0, d_measure = 1)
+tbl_category_probs.5 <- cbind(
+  tbl_transfer, 
+  as_tibble(as.data.frame(reduce(l_category_probs.5, rbind)))
+) %>% as_tibble()
+tbl_category_probs.5$prob_error <- pmap_dbl(
+  tbl_category_probs.5[, c("0", "1", "category")],
+  ~ 1 - c(..1, ..2)[as.numeric(as.character(..3)) + 1]
+)
+pl_gcm_baseline.5 <- plot_grid(tbl_category_probs.5) +
+  geom_tile(aes(fill = prob_error), alpha = .5) +
+  geom_text(aes(label = round(prob_error, 2))) +
+  scale_fill_gradient(name = "Prob. Error", low = "white", high = "tomato", limits = c(0, 1)) + 
+  geom_abline() + ggtitle("Baseline Classification, Bias = .5")
+
+# base model with bias term = .1
+l_category_probs.1 <- map(l_transfer_x, gcm_base, tbl_x = tbl_inb, n_feat = 2, c = 1, w = .5, bias = .1, delta = 0, d_measure = 1)
+tbl_category_probs.1 <- cbind(
+  tbl_transfer, 
+  as_tibble(as.data.frame(reduce(l_category_probs.1, rbind)))
+) %>% as_tibble()
+tbl_category_probs.1$prob_error <- pmap_dbl(
+  tbl_category_probs.1[, c("0", "1", "category")],
+  ~ 1 - c(..1, ..2)[as.numeric(as.character(..3)) + 1]
+)
+pl_gcm_baseline.1 <- plot_grid(tbl_category_probs.1) +
+  geom_tile(aes(fill = prob_error), alpha = .5) +
+  geom_text(aes(label = round(prob_error, 2))) +
+  scale_fill_gradient(name = "Prob. Error", low = "white", high = "tomato", limits = c(0, 1)) + 
+  geom_abline() + ggtitle("Baseline Classification, Bias = .1")
+
+# plot together
+grid.draw(arrangeGrob(pl_inb, pl_gcm_baseline.5, pl_gcm_baseline.1, nrow = 1))
+
+
+# Upsampling Example ------------------------------------------------------
+
+x_new <- tibble(x1 = 4.5, x2 = 4.7)
+tbl_x <- tbl_x_ii
+n_feat <- 2
+c <- .5
+w <- 1/n_feat # equal
+bias <- .5
+delta <- .99
+d_measure <- 1
+
+gcm_base(x_new, tbl_x, 2, c, w, bias, delta, d_measure)
+gcm_base(x_new, tbl_x, 2, c, w, bias, 0, d_measure)
+
+
+plot_grid(tbl_x) +
+  geom_abline() + coord_cartesian(xlim = c(1, 10), ylim = c(1, 10))
+
+# for one category
+categories <- c(0, 1)
+ns_upsample <- c(0, 1000)
+
+l_tbl_upsample <- map2(categories, ns_upsample, upsample, tbl_x = tbl_x)
+tbl_upsample <- l_tbl_upsample %>% reduce(rbind) %>% mutate(trial_id = seq(1, nrow(.)))
+plot_grid(tbl_upsample %>% mutate(category = factor(category)))
+gcm_base(x_new, tbl_upsample, 2, c, w, 0, d_measure)
+
+
+
+
+# Upsampling & Importance Sampling ----------------------------------------
+
+
+# upsample minority category
+# add/remove items from the set of presented items using importance sampling
+# this makes sense only in the non-decay model, because importance or weight is given by the decay parameter
+# number of finally used items is controlled by capacity parameter
+
+# example using imbalanced data set sequentially presenting data
+
+
+
+# upsample only the minority category
+categories <- c(0, 1)
+ns_upsample <- c(0, 100)
+
+l_tbl_upsample_inb <- map2(categories, ns_upsample, upsample, tbl_x = tbl_inb)
+tbl_inb_upsample <- l_tbl_upsample_inb %>% reduce(rbind) %>% 
+  group_by(x1, x2, category) %>%
+  count() %>% dplyr::select(-n) %>%
+  ungroup() %>%
+  mutate(
+    trial_id = seq(1, nrow(.))
+  )
+
+pl_inb <- plot_grid(tbl_inb %>% mutate(category = factor(category))) + ggtitle("Stimuli") + geom_abline()
+pl_inb_up <- plot_grid(tbl_inb_upsample %>% mutate(category = factor(category))) + ggtitle("Upsampled")
+grid.draw(arrangeGrob(pl_inb, pl_inb_up, nrow = 1))
+
+
+gcm_base(tibble(x1 = 2.5, x2 = 4), tbl_inb, 2, c = 1, w = .5, bias = 1/3, delta = 0, d_measure = 1)
+gcm_base(tibble(x1 = 2.5, x2 = 4), tbl_inb_upsample, 2, c = 1, w = .5, bias = 1/3, delta = 0, d_measure = 1)
+
+
+
+# todo
+# create grid of 10 x 10 values crossing x1 and x2
+# 1. run gcm with upsampled data sets (few added points - many added points)
+# 2. run gcm with bias parameter varying from unbiased to very biased and plot probabilities category 0 over 100 grid points
+
+# main remaining issue: how is upsampling exactly achieved in the model? given I want to add n data points to one category
+# possible solution: do upsampling as currently implemented in upsample function, 
+# and then rank sampled data points according to their importance for the categorization problem
+# importance sampling has to be done sequentially, otherwise points from the same neighbourhood will be sampled
+# only keep the n most important data points
+# adjust bias parameter accordingly, such that ratio of ndata(cat_0)/ndata(cat_1) is put into bias parameter
+# e.g., cat0 50, cat1 100 --> bias = c(.66, .33)
+
+# upsampling
+# need parameter capacity: nr data points upsampled
+# write function calculating likelihood of 10x10 data points
+# when each of the upsampled data points is added to the original data set
+# pick the best point and repeat process
+# pick the best point and ... --> repeat nr data points upsampled
+
+# general idea:
+# what information use people to draw inferences when they have comparatively little/much information about a category?
+# a) gcm: they just use the presented information as is
+# b) prototype: they use the average representation of the category
+# maybe just focus on hypotheses in c), because unclear from previous research whether a) or b)?
+# c) they do not use all the information presented to them
+# 1. because they forget according to a decay function
+# 2. because they strategically remember/sample some information: up-sampling & down-sampling given a capacity limit
+
+
+# varying number of upsampled points and evaluating likelihood of given responses
+# possible solution: first, upsample sufficient number of data points (e.g., 50)
+# then, do a constrained optimization with n ranging between 0 and that number (i.e., 50)
+
+
+# for one category
+categories <- c(0, 1)
+ns_upsample <- c(0, 100)
+
+l_tbl_upsample_inb <- map2(categories, ns_upsample, upsample, tbl_x = tbl_inb)
+# add small amount of noise to circumvent ties in rank odering
+tbl_inb_upsample <- l_tbl_upsample_inb %>% reduce(rbind) %>% 
+  group_by(x1, x2, category) %>%
+  count() %>% dplyr::select(-n) %>%
+  ungroup() %>%
+  mutate(
+    trial_id = seq(1, nrow(.)),
+    x1 = x1 + rnorm(nrow(.), 0, .01),
+    x2 = x2 + rnorm(nrow(.), 0, .01)
+  )
+plot_grid(tbl_inb_upsample %>% mutate(category = factor(category))) + geom_abline()
+params <- c(c = 1, w = .5, bias = .5)
+
+
+# calculate importance given set of data points
+tbl_inb_plus <- tbl_inb %>% dplyr::select(x1, x2, category)
+tbl_importance <- tbl_inb_upsample
+l_new_samples <- split(tbl_inb_upsample %>% dplyr::select(-trial_id), tbl_inb_upsample$trial_id)
+
+tbl_important_samples_2 <- importance_upsampling(l_new_samples, tbl_importance, tbl_inb_plus, params, tbl_transfer, n_feat = 2, d_measure = 1, n_max = 2)
+tbl_important_samples_2$is_new <- 1
+tbl_important_samples_2$is_new[(nrow(tbl_inb) + 1) : nrow(tbl_important_samples_2)] <- 5
+
+tbl_important_samples_10 <- importance_upsampling(l_new_samples, tbl_importance, tbl_inb_plus, params, tbl_transfer, n_feat = 2, d_measure = 1, n_max = 10)
+tbl_important_samples_10$is_new <- 1
+tbl_important_samples_10$is_new[(nrow(tbl_inb) + 1) : nrow(tbl_important_samples_10)] <- 5
+
+
+pl_points_obs <- plot_grid(tbl_inb %>% mutate(category = factor(category))) + 
+  geom_abline() + ggtitle("Observed")
+pl_points_transfer <- plot_grid(tbl_transfer) + geom_abline() + ggtitle("Transfer")
+pl_points_importance_2 <- plot_grid(tbl_important_samples_2) + geom_abline() +
+  geom_point(aes(size = is_new), shape = 1) +
+  scale_size_continuous(guide = "none") +
+  ggtitle("Add Two Samples")
+pl_points_importance_10 <- plot_grid(tbl_important_samples_10) + geom_abline() +
+  geom_point(aes(size = is_new), shape = 1) +
+  scale_size_continuous(guide = "none") +
+  ggtitle("Add Ten Samples")
+
+grid.draw(arrangeGrob(pl_points_obs, pl_points_transfer, pl_points_importance_2, pl_points_importance_10, nrow = 2))
+
+
+
+
+plot_grid(tbl_drop)
+
+# todos
+# up- and downsampling only for relevant category (i.e., lo freq and hi freq, respectively)
+# assumption: up- and downsampling are achieved using originally presented stimuli
+
+
 
 
--- utils/utils.R
+++ utils/utils.R
@@ -1,3 +1,31 @@
+plot_grid <- function(tbl) {
+  #' @description plot 2D grid of x1 and x2 data points
+    ggplot(tbl, aes(x1, x2, group = category)) + 
+    geom_point(aes(color = category)) + 
+    theme_bw() +
+    scale_x_continuous(expand = c(.1, .1), breaks = seq(2, 10, by = 2)) +
+    scale_y_continuous(expand = c(.1, .1), breaks = seq(2, 10, by = 2)) +
+    labs(x = expression(x[1]), y = expression(x[2])) +
+    theme(strip.background = element_rect(fill = "white")) +
+    scale_color_viridis_d(name = "Category")
+}
+
+
+
+sample_2d_stimuli <- function(x1, x2, category, n, sd) {
+  #' @description sample from bivariate normal
+  #' @param x1 mean 1
+  #' @param x2 mean 2
+  #' @param category a category label
+  #' @param n number of samples
+  #' @param sd the sd
+  s <- mvrnorm(n, c(x1, x2), matrix(c(sd, 0, 0, sd), nrow = 2))
+  tbl <- tibble(as.data.frame(s), category)
+  colnames(tbl) <- c("x1", "x2", "category")
+  return(tbl)
+}
+
+
 gcm_base <- function(x_new, tbl_x, n_feat, c, w, bias, delta, d_measure = 1){
   #' compute class probabilities with the GCM model
   #' 
@@ -11,6 +39,7 @@ gcm_base <- function(x_new, tbl_x, n_feat, c, w, bias, delta, d_measure = 1){
   #' @param w attentional weighting
   #' @param bias response bias / category prior
   #' @param delta forgetting rate (if delta == 0, no forgetting)
+  #' @param d_measure distance measure, 1 for city-block, 2 for euclidean
   #' @return a vector with the class probabilities for the new item
   w <- c(w, 1 - w)
   bias <- c(bias, 1 - bias)
@@ -21,6 +50,7 @@ gcm_base <- function(x_new, tbl_x, n_feat, c, w, bias, delta, d_measure = 1){
   map_dbl(sims_cat_sum_biased, ~ .x/sum(sims_cat_sum_biased))
 }
 
+
 f_similarity <- function(x1, x2, w, c, x_new, d_measure) {
   #' @description helper function calculating similarity for one item
   #' given a currently presented item to be classified
@@ -29,6 +59,7 @@ f_similarity <- function(x1, x2, w, c, x_new, d_measure) {
   )^(1/d_measure)
   exp(-d*c)
 }
+
 
 f_similarity_cat <- function(x, w, c, delta, x_new, d_measure) {
   #' @description helper function calculating similarities for all items
@@ -39,12 +70,14 @@ f_similarity_cat <- function(x, w, c, delta, x_new, d_measure) {
   return(sims*x$prop_decay)
 }
 
+
 my_weighted_sample <- function(prop_1, tbl_df) {
   tibble(
     x1 = tbl_df$x1_1 * prop_1 + tbl_df$x1_2 * (1 - prop_1),
     x2 = tbl_df$x2_1 * prop_1 + tbl_df$x2_2 * (1 - prop_1)
   )
 }
+
 
 upsample <- function(category_used, n_upsample, tbl_x) {
   #' upsample by linearly interpolating between observed data points
@@ -88,150 +121,15 @@ upsample <- function(category_used, n_upsample, tbl_x) {
 }
 
 
-
-
-
-x_new <- tibble(x1 = 4.5, x2 = 4.7)
-tbl_x <- tbl_x_ii
-n_feat <- 2
-c <- .5
-w <- 1/n_feat # equal
-bias <- .5
-delta <- .99
-d_measure <- 1
-
-gcm_base(x_new, tbl_x, 2, c, w, bias, delta, d_measure)
-gcm_base(x_new, tbl_x, 2, c, w, bias, 0, d_measure)
-
-
-plot_grid(tbl_x) +
-  geom_abline() + coord_cartesian(xlim = c(1, 10), ylim = c(1, 10))
-
-# for one category
-categories <- c(0, 1)
-ns_upsample <- c(0, 1000)
-
-l_tbl_upsample <- map2(categories, ns_upsample, upsample, tbl_x = tbl_x)
-tbl_upsample <- l_tbl_upsample %>% reduce(rbind) %>% mutate(trial_id = seq(1, nrow(.)))
-plot_grid(tbl_upsample %>% mutate(category = factor(category)))
-gcm_base(x_new, tbl_upsample, 2, c, w, 0, d_measure)
-
-
-
-# todos
-# add/remove an item from the set of presented items
-# and test how much that changes the fit
-# this makes sense only in the non-decay model
-# because recently added items have more weight in the decay model
-
-
-# number of finally used items is controlled by capacity parameter
-
-tbl_x_ii_inb$response <- tbl_x_ii_inb$category
-
-# category 0 example
-gcm_base(tbl_x_ii_inb[1, c("x1", "x2")], tbl_x_ii_inb[-1, ], 2, c = .5, w = c(.5, .5), .5, 0, d_measure = 1)
-# category 1 example
-gcm_base(tbl_x_ii_inb[17, c("x1", "x2")], tbl_x_ii_inb, 2, c = 3, w = c(.001, .999), .5, 0, d_measure = 1)
-
-
-# example using inbalanced data set sequentially presenting data
-
-tbl_inb <- tbl_x_ii %>% 
-  filter(
-    category %in% c(0) |
-      (category %in% c(1) & x1 %in% seq(1, 9, by = 4) & x2 %in% seq(2, 10, by = 4))
-  )
-plot_grid(tbl_inb) + geom_abline()
-
-# for one category
-categories <- c(0, 1)
-ns_upsample <- c(0, 100)
-
-l_tbl_upsample_inb <- map2(categories, ns_upsample, upsample, tbl_x = tbl_inb)
-tbl_inb_upsample <- l_tbl_upsample_inb %>% reduce(rbind) %>% 
-  group_by(x1, x2, category) %>%
-  count() %>% dplyr::select(-n) %>%
-  ungroup() %>%
-  mutate(
-    trial_id = seq(1, nrow(.))
-    )
-
-pl_inb <- plot_grid(tbl_inb %>% mutate(category = factor(category))) + ggtitle("Stimuli") + geom_abline()
-pl_inb_up <- plot_grid(tbl_inb_upsample %>% mutate(category = factor(category))) + ggtitle("Upsampled")
-grid.draw(arrangeGrob(pl_inb, pl_inb_up, nrow = 1))
-
-
-gcm_base(tibble(x1 = 2.5, x2 = 4), tbl_inb, 2, c = 1, w = .5, bias = 1/3, delta = 0, d_measure = 1)
-gcm_base(tibble(x1 = 2.5, x2 = 4), tbl_inb_upsample, 2, c = 1, w = .5, bias = 1/3, delta = 0, d_measure = 1)
-
-# todo
-# create grid of 10 x 10 values crossing x1 and x2
-# 1. run gcm with upsampled data sets (few added points - many added points)
-# 2. run gcm with bias parameter varying from unbiased to very biased and plot probabilities category 0 over 100 grid points
-
-# main remaining issue: how is upsampling exactly achieved in the model? given I want to add n data points to one category
-# possible solution: do upsampling as currently implemented in upsample function, 
-# and then rank sampled data points according to their importance for the categorization problem
-# importance sampling has to be done sequentially, otherwise points from the same neighbourhood will be sampled
-# only keep the n most important data points
-# adjust bias parameter accordingly, such that ratio of ndata(cat_0)/ndata(cat_1) is put into bias parameter
-# e.g., cat0 50, cat1 100 --> bias = c(.66, .33)
-
-# upsampling
-# need parameter capacity: nr data points upsampled
-# write function calculating likelihood of 10x10 data points
-# when each of the upsampled data points is added to the original data set
-# pick the best point and repeat process
-# pick the best point and ... --> repeat nr data points upsampled
-
-
-
-# create 10 x 10 grid of data points
-xs <- seq(1, 10, by = 1)
-tbl_transfer <- crossing(x1 = xs, x2 = xs) %>% mutate(category = fct_rev(factor(x1 > x2, labels = c(1, 0))))
-l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
-plot_grid(tbl_transfer) + geom_abline()
-
-gcm_base(tbl_transfer[10, c("x1", "x2")], tbl_inb, n_feat = 2, c = 1, w = .5, bias = .5, delta = 0, d_measure = 1)
-
-
-# base model with bias term = .5
-l_category_probs.5 <- map(l_transfer_x, gcm_base, tbl_x = tbl_inb, n_feat = 2, c = 1, w = .5, bias = .5, delta = 0, d_measure = 1)
-tbl_category_probs.5 <- cbind(
-  tbl_transfer, 
-  as_tibble(as.data.frame(reduce(l_category_probs.5, rbind)))
-) %>% as_tibble()
-tbl_category_probs.5$prob_error <- pmap_dbl(
-  tbl_category_probs.5[, c("0", "1", "category")],
-  ~ 1 - c(..1, ..2)[as.numeric(as.character(..3)) + 1]
-)
-pl_gcm_baseline.5 <- plot_grid(tbl_category_probs.5) +
-  geom_tile(aes(fill = prob_error), alpha = .5) +
-  scale_fill_gradient(name = "Prob. Error", low = "white", high = "tomato", limits = c(0, 1)) + 
-  geom_abline() + ggtitle("Baseline Classification, Bias = .5")
-
-# base model with bias term = .1
-l_category_probs.1 <- map(l_transfer_x, gcm_base, tbl_x = tbl_inb, n_feat = 2, c = 1, w = .5, bias = .1, delta = 0, d_measure = 1)
-tbl_category_probs.1 <- cbind(
-  tbl_transfer, 
-  as_tibble(as.data.frame(reduce(l_category_probs.1, rbind)))
-) %>% as_tibble()
-tbl_category_probs.1$prob_error <- pmap_dbl(
-  tbl_category_probs.1[, c("0", "1", "category")],
-  ~ 1 - c(..1, ..2)[as.numeric(as.character(..3)) + 1]
-)
-pl_gcm_baseline.1 <- plot_grid(tbl_category_probs.1) +
-  geom_tile(aes(fill = prob_error), alpha = .5) +
-  scale_fill_gradient(name = "Prob. Error", low = "white", high = "tomato", limits = c(0, 1)) + 
-  geom_abline() + ggtitle("Baseline Classification, Bias = .1")
-
-# plot together
-grid.draw(arrangeGrob(pl_inb, pl_gcm_baseline.5, pl_gcm_baseline.1, nrow = 1))
-
-
-
 gcm_likelihood_no_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measure) {
+  #' @description -2 * negative log likelihood of transfer set given training data
+  #' and a gcm without a forgetting parameter (i.e., forgetting set to 0)
+  #' @param x parameters
+  #' @param tbl_transfer transfer/test data
+  #' @param tbl_x training data
+  #' @param n_feat number of features
+  #' @param d_measure distance measure, 1 for city-block, 2 for euclidean
+  #' @return negative 2 * summed log likelihood
   l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
   l_category_probs <- map(l_transfer_x, gcm_base, tbl_x = tbl_x, n_feat = n_feat, c = x[[1]], w = x[[2]], bias = x[[3]], delta = 0, d_measure = d_measure)
   tbl_probs <- as.data.frame(reduce(l_category_probs, rbind)) %>% mutate(category = tbl_transfer$category)
@@ -244,7 +142,16 @@ gcm_likelihood_no_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measu
   return(neg2llsum)
 }
 
+
 gcm_likelihood_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measure) {
+  #' @description -2 * negative log likelihood of transfer set given training data
+  #' and a gcm with a forgetting parameter
+  #' @param x parameters
+  #' @param tbl_transfer transfer/test data
+  #' @param tbl_x training data
+  #' @param n_feat number of features
+  #' @param d_measure distance measure, 1 for city-block, 2 for euclidean
+  #' @return negative 2 * summed log likelihood
   l_transfer_x <- split(tbl_transfer[, c("x1", "x2")], 1:nrow(tbl_transfer))
   l_category_probs <- map(l_transfer_x, gcm_base, tbl_x = tbl_x, n_feat = n_feat, c = x[[1]], w = x[[2]], bias = x[[3]], delta = x[[4]], d_measure = d_measure)
   tbl_probs <- as.data.frame(reduce(l_category_probs, rbind)) %>% mutate(category = tbl_transfer$category)
@@ -258,41 +165,34 @@ gcm_likelihood_forgetting <- function(x, tbl_transfer, tbl_x, n_feat, d_measure)
 }
 
 
-# for one category
-categories <- c(0, 1)
-ns_upsample <- c(0, 100)
-
-l_tbl_upsample_inb <- map2(categories, ns_upsample, upsample, tbl_x = tbl_inb)
-tbl_inb_upsample <- l_tbl_upsample_inb %>% reduce(rbind) %>% 
-  group_by(x1, x2, category) %>%
-  count() %>% dplyr::select(-n) %>%
-  ungroup() %>%
-  mutate(
-    trial_id = seq(1, nrow(.)),
-    x1 = x1 + rnorm(nrow(.), 0, .01),
-    x2 = x2 + rnorm(nrow(.), 0, .01)
+add_sample <- function(tbl_new_sample, tbl_base, params, tbl_transfer, n_feat, d_measure, f_likelihood) {
+  #' @description evaluate likelihood on transfer/test set when a new sample is added to the training data
+  f_likelihood(
+    params, tbl_transfer, 
+    tbl_x = rbind(tbl_base, tbl_new_sample) %>% mutate(trial_id = 1:(nrow(tbl_base) + 1)),
+    n_feat = 2, d_measure = 1
     )
-plot_grid(tbl_inb_upsample %>% mutate(category = factor(category))) + geom_abline()
-params <- c(c = 1, w = .5, bias = .5)
-
-
-add_sample <- function(tbl_new_sample, tbl_base, params, tbl_transfer, n_feat, d_measure) {
-  gcm_likelihood_no_forgetting(params, tbl_transfer, tbl_x = rbind(tbl_base, tbl_new_sample) %>% mutate(trial_id = 1:(nrow(tbl_base) + 1)), n_feat = 2, d_measure = 1)
 }
 
 
+remove_sample <- function(rwn_remove, tbl_base, params, tbl_transfer, n_feat, d_measure, f_likelihood) {
+  #' @description evaluate likelihood on transfer/test set when one training example is removed
+  f_likelihood(params, tbl_transfer, tbl_x = tbl_base[-rwn_remove, ] %>% mutate(trial_id = 1:(nrow(tbl_base) - 1)), n_feat = 2, d_measure = 1)
+}
 
-
-# varying number of upsampled points and evaluating likelihood of given responses
-# possible solution: first, upsample sufficient number of data points (e.g., 50)
-# then, do a constrained optimization with n ranging between 0 and that number (i.e., 50)
 
 importance_upsampling <- function(l_new_samples, tbl_importance, tbl_inb_plus, params, tbl_transfer, n_feat, d_measure, n_max = 10) {
+  #' @description add n_max most important upsampled data points to the training set
+  #' 
   future::plan(multisession, workers = future::availableCores() - 2)
   l_samples <- l_new_samples
   # sequential importance sampling
   for (i in 1:n_max) {
-    v_importance <- future_map_dbl(l_samples, add_sample, tbl_base = tbl_inb_plus, params = params, tbl_transfer = tbl_transfer, n_feat = 2, d_measure = 1)
+    v_importance <- future_map_dbl(
+      l_samples, add_sample, tbl_base = tbl_inb_plus, params = params, 
+      tbl_transfer = tbl_transfer, n_feat = 2, d_measure = 1,
+      f_likelihood = gcm_likelihood_no_forgetting
+      )
     tbl_importance$importance <- v_importance
     # pick best imagined data point
     tbl_importance <- tbl_importance %>% mutate(rank_importance = rank(importance, ties.method = "min"))
@@ -306,47 +206,20 @@ importance_upsampling <- function(l_new_samples, tbl_importance, tbl_inb_plus, p
   future::plan("default")
   return(tbl_inb_plus)
 }
-# calculate importance given set of data points
-tbl_inb_plus <- tbl_inb %>% dplyr::select(x1, x2, category)
-tbl_importance <- tbl_inb_upsample
-l_new_samples <- split(tbl_inb_upsample %>% dplyr::select(-trial_id), tbl_inb_upsample$trial_id)
-
-tbl_important_samples_2 <- importance_upsampling(l_new_samples, tbl_importance, tbl_inb_plus, params, tbl_transfer, n_feat = 2, d_measure = 1, n_max = 2)
-tbl_important_samples_2$is_new <- 1
-tbl_important_samples_2$is_new[(nrow(tbl_inb) + 1) : nrow(tbl_important_samples_2)] <- 5
-
-tbl_important_samples_10 <- importance_upsampling(l_new_samples, tbl_importance, tbl_inb_plus, params, tbl_transfer, n_feat = 2, d_measure = 1, n_max = 10)
-tbl_important_samples_10$is_new <- 1
-tbl_important_samples_10$is_new[(nrow(tbl_inb) + 1) : nrow(tbl_important_samples_10)] <- 5
-
-
-pl_points_obs <- plot_grid(tbl_inb %>% mutate(category = factor(category))) + 
-  geom_abline() + ggtitle("Observed")
-pl_points_transfer <- plot_grid(tbl_transfer) + geom_abline() + ggtitle("Transfer")
-pl_points_importance_2 <- plot_grid(tbl_important_samples_2) + geom_abline() +
-  geom_point(aes(size = is_new), shape = 1) +
-  scale_size_continuous(guide = "none") +
-  ggtitle("Add Two Samples")
-pl_points_importance_10 <- plot_grid(tbl_important_samples_10) + geom_abline() +
-  geom_point(aes(size = is_new), shape = 1) +
-  scale_size_continuous(guide = "none") +
-  ggtitle("Add Ten Samples")
-
-grid.draw(arrangeGrob(pl_points_obs, pl_points_transfer, pl_points_importance_2, pl_points_importance_10, nrow = 2))
-
-
-remove_sample <- function(rwn_remove, tbl_base, params, tbl_transfer, n_feat, d_measure) {
-  gcm_likelihood_no_forgetting(params, tbl_transfer, tbl_x = tbl_base[-rwn_remove, ] %>% mutate(trial_id = 1:(nrow(tbl_base) - 1)), n_feat = 2, d_measure = 1)
-}
-
 
 
 importance_downsampling <- function(tbl_inb_plus, params, tbl_transfer, n_feat, d_measure, n_max = 10) {
+  #' @description remove n_max least important upsampled data points from the training set
+  #' 
   future::plan(multisession, workers = future::availableCores() - 2)
   # sequential importance sampling
   tbl_drop <- tbl_inb_plus
   while (nrow(tbl_drop) > n_max) {
-    v_importance <- future_map_dbl(1:nrow(tbl_drop), remove_sample, tbl_base = tbl_drop, params = params, tbl_transfer = tbl_transfer, n_feat = 2, d_measure = 1)
+    v_importance <- future_map_dbl(
+      1:nrow(tbl_drop), remove_sample, tbl_base = tbl_drop, params = params, 
+      tbl_transfer = tbl_transfer, n_feat = 2, d_measure = 1,
+      f_likelihood = gcm_likelihood_no_forgetting
+      )
     tbl_drop$importance <- v_importance
     # pick best imagined data point
     tbl_drop <- tbl_drop %>% mutate(rank_importance = rank(importance, ties.method = "min"))
@@ -358,9 +231,3 @@ importance_downsampling <- function(tbl_inb_plus, params, tbl_transfer, n_feat, 
   future::plan("default")
   return(tbl_drop)
 }
-
-plot_grid(tbl_drop)
-
-
-
-
